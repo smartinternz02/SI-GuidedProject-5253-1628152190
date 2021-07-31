736db1e08d77e7c01c6634d97f3ddf4686baacf2
sandhya-art <82027355+sandhya-art@users.noreply.github.com>
2021-07-31 16:08:20 +0530

    Delete SpringBootApp-master/SpringBootApp-master directory

--- SpringBootApp-master/SpringBootApp-master/src/main/java/com/chanucodes/Smart_Id_Scanner/UserRepositry.java
+++
@@ -1,8 +0,0 @@
-package com.chanucodes.Smart_Id_Scanner;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositry extends JpaRepository<User,String> {
-
-	User findByEmail(String email);
-}
